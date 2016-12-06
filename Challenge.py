c1af7317142f886617647af71f549337a16188aa
Cr1s19 <alexsis@gmail.com>
2016-12-05 20:28:46 -0600

    id3 completed, with new functions that work better and optimally thanks to the study of lambda, better split of datasets and the implementation of classifier function

--- Challenge.py
+++ Challenge.py
@@ -6,216 +6,134 @@ import array
 import itertools
 import operator
 import collections
-from ete3 import Tree
+from collections import Counter
+from ete3 import PhyloTree
 
+# struct node to make the desicion tree
+class node:
+    def __init__(self,col=-1,value=None,results=None,accepted=None,rejected=None):
+        self.col=col
+        self.value=value
+        self.results=results
+        self.accepted=accepted
+        self.rejected=rejected
+
+# read the dataset
 def reading():
     inputs = [];
-    last_input = [];
     for line in sys.stdin:
         result = line.split(',');
         inputs.append(result);
     return inputs
 
-# Get the values of one column in specific index
-def getColumn(list,index):
-    column = [];
-    for x in list:
-        column.append(x[index])
-    return column
-
-# Get the number of equals values in a column
-def getEquals(list, string, index):
-    v = getColumn(list,index)
-    count = 0
-    for x in v:
-        if x == string:
-            count +=1
-    return count
-
-# Get equals in a specific columns compairing pairs
-def getEquals_Column(list,pair):
-    count = 0
-    for x in list:
-        if x == pair:
-            count +=1
-    return count
-
-# Get numbers of differents types in a column given the entire list with an especific index
-def getDifferents(list,index):
-    v = getColumn(list,index)
-    types = []
-    for x in v:
-        if types.count(x) >= 1:
-            continue
-        else:
-            types.append(x)
-    return types
-
-# Get the values that are repeated in the column given by the index
-def getDifferents_Column(column):
-    types = []
-    for x in column:
-        if types.count(x) >= 1:
-            continue
-        else:
-            types.append(x)
-    return types
-
-#Get the total number of data
-def getTotal(list):
-    count = 0
-    for x in list:
-        count += 1
-    return count;
-
-# Get the total of columns in the data set
-def getTotal_Column(list):
-    count = 0
-    for x in list:
-        for y in x:
-            count += 1
-        break
-    return count
-
-# Get the probability of one value
-def P(list,string,total,index):
-    count = getEquals(list,string,index)
-    return float(count)/float(total);
-
-# Get the probability of one value in a specific column comapared by pairs
+# Get the probability of one value in a specific column
 def P_Column(count,total):
     return float(count)/float(total);
 
-# Make pairs for the first column and the second column given by the indexes
-def makePairs(list,index,indexSecond):
-    column1 = getColumn(list,index)
-    column2 = getColumn(list,indexSecond)
-    values_dict = []
-    for x,y in zip(column1,column2):
-        value = {x:y}
-        values_dict.append(value);
-    return values_dict
+# Get the number of equals values in a row
+def getEquals(data):
+    counts={}
+    for x in data:
+        v=x[len(x)-1]
+        if v not in counts:
+            counts[v]=0
+        counts[v]+=1
+    return counts
 
-# Get the General Entropy
-def H(list,total,index):
-    strings_total = getDifferents(list,index)
-    sumatory = 0
-    for x in strings_total:
-        p = P(list,x,total,index)
-        sumatory -= p*math.log(p,2)
-    return sumatory
+# Divides a dataset on by a specific column by function to detect values that are equal to the given value
+def split(data,index,value):
+    split_function=lambda row:row[index]==value
+    dataset1=[row for row in data if split_function(row)]
+    dataset2=[row for row in data if not split_function(row)]
+    return (dataset1,dataset2)
 
-# Get the Entropy of the sum in the values of acolumn compared by another column given by indexes
-def HI(list,total,index,indexSecond):
-    getDifferents(list,index);
-    values_dict = makePairs(list,index,indexSecond)
-    v = getDifferents_Column(values_dict)
-    p = 0
-    count = 0
-    p_total = 0
-    sub_total = 0
-    sumatory = 0
-    for x in v:
-        count = getEquals_Column(values_dict,x)
-        sub_total = getEquals(list,x.keys()[0],index)
-        p = P_Column(count,sub_total)
-        p_total = P(list,x.keys()[0],total,index)
-        sumatory -= p_total*(p*math.log(p,2))
-    return sumatory
+# Get the entropy by the count of each value in the row
+def H(data):
+    entropy=0
+    values=getEquals(data)
+    for x in values.keys():
+        p=float(values[x])/len(data)
+        entropy-=p*math.log(p,2)
+    return entropy
 
-# Get the information gain of the indexes given
-def IG(list,total,index,indexSecond):
-    h = H(list,total,indexSecond)
-    hi = HI(list,total,index,indexSecond)
-    result = h-hi
-    return result
+#Count the number of attributes in the row less the objective value.
+def getAttributes_Column(data):
+    count=len(data[0])-1
+    return count
 
-# Get the greatest information gain
-def getGratestIG(list):
-    if not list:
-        return 0
-    else:
-        return max(list)
-
-# Sort the data set given the IG
-# def SortIG(list,index):
-#     for x in list:
-#         value = sorted(list,key=operator.itemgetter(index));
-#     return value
-
-# Split the dataSet given the IG
-def SplitIG(list,value,index):
-    splittables = []
-    for x in list:
-        if x[index] == value:
-            splittables.append(x)
-    return splittables
-
-def SplitIG_without_index(list,index):
-    data = []
-    for x in list:
-        for y in x:
-            y.pop(index)
-            data.append(y)
-    return data
-
-def checkEquals(list):
-   return len(set(list)) <= 1
-
-def id3(list,total,indexI):
-    total_horizontal = getTotal_Column(list)
-    total_vertical = getTotal(list)
-    values = []
-    valuesColumn = []
-    ig = 0
-    index = 0
-    sortSplit = []
-    sortSplitIndex = []
-    sortData = []
-    sortDataIndex = []
-    newDataSet = []
-
-    tree = lambda: collections.defaultdict(tree)
-    root = Tree()
-
-    for x in xrange(0,total_horizontal-1):
-        ig = IG(list,total_vertical,x,-1)
-        values.append(ig)
+# Execution of the id3 algorithm and creating the desicion tree
+def ID3(data,root):
+    best_IG=0.0
+    best_criteria=None
+    best_datasets=None
+    column_values={}
     
-    if values:
-        if not checkEquals(values):
-            print values
-            ggi = getGratestIG(values)
-            for i,x in enumerate(values):
-                if x == ggi:
-                    index = i
+    if len(data) == 0:
+        return node()
+    
+    H_general = H(data)
+    column_without_objective= getAttributes_Column(data)
 
-            valuesColumn = getDifferents(list,index)
-            for x in valuesColumn:
-                sortSplit = SplitIG(list,x,index)
-                sortData.append(sortSplit)
-            
-            sortDataIndex = SplitIG_without_index(sortData,index)
-            print index
+    for column_index in range(0,column_without_objective):
+        # check visited with key as the value given by the index and value = 42
+        for x in data:
+            column_values[x[column_index]]=42
 
-            if index > indexI:
-                indexI=index+1
-            else:
-                indexI = index
+        # check the values of the dataset and make the split with the IG of each new dataset
+        for value in column_values.keys():
+            (dataset1,dataset2)=split(data,column_index,value)
+            # Get the probabilities
+            p = P_Column(len(dataset1),len(data))
+            p_ = 1-p
+            # Get the IG given the datasets chossed by the value
+            IG = H_general-p*H(dataset1)-p_*H(dataset2)
+            # Get the greates IG of all the data set
+            if IG>best_IG and len(dataset1)>0 and len(dataset2)>0:
+                best_IG = IG
+                best_criteria = (column_index,value)
+                best_datasets = (dataset1,dataset2)
+    
+    # Check if the IG can be splited; if best_IG > 0 the dataset can continue to be splited in other case it stoped becouse it is pure
+    if best_IG>0:
+        # node to print the tree
+        A =root.add_child(name = value)
+        # Recursive call with the accepted value path and rejected value path
+        accepted=ID3(best_datasets[0],A)
+        not_accepted=ID3(best_datasets[1],A)
+        # return a node with the values in the actual iteration
+        return node(col=best_criteria[0],value=best_criteria[1],accepted=accepted,rejected=not_accepted)
+    else:
+        # returns result values of the leafs
+        return node(results=getEquals(data))
 
-            print "indextI", indexI
-            root.add_child(name=indexI)
-            for x in sortData:
-                print x
-                total = getTotal(x)
-                id3(x,total,indexI)
+# Classify the new information
+def Classifier(data,desiciontree):
+    if desiciontree.results!=None:
+        return desiciontree.results
+    else:
+        # check the value with the index of the column in the branch if it is accepted or rejected
+        v=data[desiciontree.col]
+        path = None
+        if v == desiciontree.value:
+            path = desiciontree.accepted
+        else:
+            path = desiciontree.rejected
+        return Classifier(data, path)
 
 def main():
     input_data = reading()
-    t = getTotal(input_data)
-    id3(input_data,t,42)
+    new_data = []
+    root = PhyloTree()
+    tree = ID3(input_data,root)
+    print root
+    # file = open("verifyData.txt", "r+")
+    with open("verifyData.txt") as f:
+        for line in f:
+            result = line.split(',');
+            new_data.append(result)
 
+    for data in new_data:
+        print Classifier(data,tree)
 
- 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Classification_Challenge
 Using a classification algorithm the program going to classify a dataset in a decision tree.
+
+Note: 
+	In order to Run this program you need to install et3 here it is the instrucions of install it: http://etetoolkit.org/download/
+
+The form to use it is the next:
+	In your terminal enter in the directory where it is the archive Challeng.py
+	Then run the program wiht:
+		$ python Challenge.py
+
+If you want to modify the dataset enter to the input_02_connect-4.data and add or delete the line of the data set of yout chose
+If you want to classify a certain dataset add it to the file verifiyData.txt with the necessary format to make a proper classification.
